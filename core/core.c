4e9ed83f833bc0b4962a59950f866d8bac143351
Michail Rusinov <marusinov@gmail.com>
2014-07-04 16:57:44 +0400

    Playing with Core...

--- core/core.c
+++ core/core.c
@@ -2,13 +2,39 @@
 #include "hal.h"
 #include "core.h"
 
+static msg_t core_msg_b;
+
 void Core_Init()
 {
 	Core_Base.id = 0;
 	Core_Base.type = Base;
 //	Core_Base.addr = MY_ADDR;
-	Core_Base.mbox = NULL;
+	Core_Base.mbox = &core_mb;
 	Core_Base.direction = None;
 	Core_Base.next = NULL;
 	Core_Base.description = "Test Board 1\0";
+}
+
+THD_WORKING_AREA(waCore, 128);
+//__attribute__((noreturn))
+THD_FUNCTION(Core,arg)
+{
+	(void) arg;
+	chRegSetThreadName("Core");
+
+	Core_Init();
+	core_base_struct_t *current;
+	current = &Core_Base;
+	chMBObjectInit(&core_mb, core_mb_b, 1);
+
+	while (TRUE)
+	{
+//		chMBFetch(&core_mb, (msg_t *) &tx_buffer, TIME_INFINITE);
+	}
+}
+
+void Core_Start()
+{
+	chThdCreateStatic(waCore, sizeof(waCore), NORMALPRIO,
+			Core, NULL);
 }
