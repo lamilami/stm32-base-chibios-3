5986ef530d107250dff314f29ea801824c0f56b7
Michail Rusinov <marusinov@gmail.com>
2014-07-29 16:59:07 +0400

    Playing with RTC

--- cmsis_boot/stm32f0xx_conf.h
+++ cmsis_boot/stm32f0xx_conf.h
@@ -46,7 +46,6 @@
 #include "stm32f0xx_iwdg.h"
 #include "stm32f0xx_pwr.h" 
 #include "stm32f0xx_rcc.h" 
-#include "stm32f0xx_rtc.h" 
 #include "stm32f0xx_spi.h"
 #include "stm32f0xx_tim.h"
 #include "stm32f0xx_usart.h" 
@@ -60,6 +59,7 @@
 #include "stm32f0xx_syscfg.h"
 #include "stm32f0xx_usart.h"
 #include "stm32f0xx_dma.h"
+#include "stm32f0xx_rtc.h"
 
 /* Exported types ------------------------------------------------------------*/
 /* Exported constants --------------------------------------------------------*/
