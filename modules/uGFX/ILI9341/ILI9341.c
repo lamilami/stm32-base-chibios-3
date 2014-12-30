b5d3f7f29a2d393f03eff48b1a9f239ac4ff9250
Michail Rusinov <marusinov@gmail.com>
2014-12-31 00:10:21 +0400

    Graph works!!!

--- modules/uGFX/ILI9341/ILI9341.c
+++ modules/uGFX/ILI9341/ILI9341.c
@@ -2,15 +2,15 @@
 #include "hal.h"
 #include "core.h"
 
-#if ILI9341_PRESENT
+//#if ILI9341_PRESENT
+#if 1
 
 #include "printf.h"
 
-graph_t Graph_Data= {};
+graph_t Graph_Data = {};
 
 static thread_t *tp;
 #define ALARM_PERIOD_MS 5000
-
 #define NUMBERS_WIDTH 35
 #define NUMBERS_HEIGHT 24
 #define HEIGHT_24   24
@@ -21,24 +21,26 @@ static thread_t *tp;
 #define OT_BG_COLOR HTML2COLOR(0x660066)
 #define TEMP_BG_COLOR   HTML2COLOR(0x820000)
 #define HUM_BG_COLOR    HTML2COLOR(0x003264)
-#define TEXT_1_COLOR    Red
+#define TEXT_1_COLOR    HTML2COLOR(0x00FFFF)
 #define TEXT_2_COLOR    Yellow
 #define TEXT_3_COLOR    HTML2COLOR(0x00B050)
+COLOR_TYPE Text_Color[3] = {TEXT_1_COLOR, TEXT_2_COLOR, TEXT_3_COLOR};
+COLOR_TYPE BG_Color[2] = {TEMP_BG_COLOR, HUM_BG_COLOR};
 
 static void rtc_cb(RTCDriver *rtcp, rtcevent_t event) {
 
   (void)rtcp;
 
   switch (event) {
-    case RTC_EVENT_OVERFLOW:
+  case RTC_EVENT_OVERFLOW:
     break;
-    case RTC_EVENT_SECOND:
+  case RTC_EVENT_SECOND:
     /* Wakes up the thread.*/
     chSysLockFromISR();
     chEvtSignalI(tp, (eventmask_t)1);
     chSysUnlockFromISR();
     break;
-    case RTC_EVENT_ALARM:
+  case RTC_EVENT_ALARM:
     /* Wakes up the thread.*/
     chSysLockFromISR();
     chEvtSignalI(tp, (eventmask_t)2);
@@ -56,7 +58,7 @@ THD_FUNCTION(ILI9341,arg) {
   tp = chThdGetSelfX();
 
   coord_t width, height;
-//  coord_t i, j;
+  //  coord_t i, j;
 
   /*
    * This initialization requires the OS already active because it uses delay
@@ -101,21 +103,23 @@ THD_FUNCTION(ILI9341,arg) {
 //  time_start = chVTGetSystemTime();
 //  uint32_t seconds = 12, min = 49, hours = 11;
   struct tm DT_StructTM, DT_StructTM_old;
+  /*
+   DT_StructTM.tm_year = 114;
+   DT_StructTM.tm_mon = 11;
+   DT_StructTM.tm_mday = 22;
+   DT_StructTM.tm_isdst = 0;
+   DT_StructTM.tm_wday = 0;
 
-  DT_StructTM.tm_year = 114;
-  DT_StructTM.tm_mon = 11;
-  DT_StructTM.tm_mday = 22;
-  DT_StructTM.tm_isdst = 0;
-  DT_StructTM.tm_wday = 0;
+   DT_StructTM.tm_hour = 20;
+   DT_StructTM.tm_min = 17;
+   DT_StructTM.tm_sec = 07;
 
-  DT_StructTM.tm_hour = 20;
-  DT_StructTM.tm_min = 17;
-  DT_StructTM.tm_sec = 07;
+   DT_StructTM_old = DT_StructTM;
+   */
 
-  DT_StructTM_old = DT_StructTM;
-  DT_StructTM_old.tm_sec--;
-  DT_StructTM_old.tm_min--;
-  DT_StructTM_old.tm_hour--;
+  DT_StructTM_old.tm_sec = -1;
+  DT_StructTM_old.tm_min = -1;
+  DT_StructTM_old.tm_hour = -1;
 
   RTCDateTime DateTime;
 //  RTCAlarm AlarmTime;
@@ -132,26 +136,26 @@ THD_FUNCTION(ILI9341,arg) {
 
   gdispClear(BG_COLOR);
 
-  gdispFillStringBox(0, 0, width, HEIGHT_24, "GrowBox", font24, TEXT_COLOR, BG_COLOR, justifyCenter);
+  gdispFillStringBox(0, 0, width, HEIGHT_24, "GrowBox", font24, Red, BG_COLOR, justifyCenter);
   gdispFillStringBox(0, HEIGHT_24, width, HEIGHT_32, "Out. Temp:", font32, TEXT_COLOR, OT_BG_COLOR, justifyLeft);
 
   gdispFillStringBox(0, HEIGHT_24 + HEIGHT_32, width / 2, HEIGHT_24, "Temp 1:", font24, TEXT_1_COLOR, TEMP_BG_COLOR,
-      justifyLeft);
+                     justifyLeft);
   gdispFillStringBox(0, HEIGHT_24 * 2 + HEIGHT_32, width / 2, HEIGHT_24, "Temp 2:", font24, TEXT_2_COLOR, TEMP_BG_COLOR,
-      justifyLeft);
+                     justifyLeft);
   gdispFillStringBox(0, HEIGHT_24 * 3 + HEIGHT_32, width / 2, HEIGHT_24, "Temp 3:", font24, TEXT_3_COLOR, TEMP_BG_COLOR,
-      justifyLeft);
+                     justifyLeft);
   gdispFillArea(0, HEIGHT_24 * 4 + HEIGHT_32, width / 2, height - (HEIGHT_24 * 4 + HEIGHT_32 + NUMBERS_HEIGHT),
-      TEMP_BG_COLOR);
+                TEMP_BG_COLOR);
 
   gdispFillStringBox(width / 2, HEIGHT_24 + HEIGHT_32, width / 2, HEIGHT_24, "Hum 1:", font24, TEXT_1_COLOR,
-      HUM_BG_COLOR, justifyLeft);
+                     HUM_BG_COLOR, justifyLeft);
   gdispFillStringBox(width / 2, HEIGHT_24 * 2 + HEIGHT_32, width / 2, HEIGHT_24, "Hum 2:", font24, TEXT_2_COLOR,
-      HUM_BG_COLOR, justifyLeft);
+                     HUM_BG_COLOR, justifyLeft);
   gdispFillStringBox(width / 2, HEIGHT_24 * 3 + HEIGHT_32, width / 2, HEIGHT_24, "Hum 3:", font24, TEXT_3_COLOR,
-      HUM_BG_COLOR, justifyLeft);
+                     HUM_BG_COLOR, justifyLeft);
   gdispFillArea(width / 2, HEIGHT_24 * 4 + HEIGHT_32, width / 2, height - (HEIGHT_24 * 4 + HEIGHT_32 + NUMBERS_HEIGHT),
-      HUM_BG_COLOR);
+                HUM_BG_COLOR);
 
   /*
    *   Using GWin
@@ -179,64 +183,151 @@ THD_FUNCTION(ILI9341,arg) {
       sprintf_(buf, "%02u:", DT_StructTM.tm_hour);
       DT_StructTM_old.tm_hour = DT_StructTM.tm_hour;
       gdispFillStringBox(width - NUMBERS_WIDTH * 3 - COLON_WIDTH * 2, (height - NUMBERS_HEIGHT),
-          NUMBERS_WIDTH + COLON_WIDTH, NUMBERS_HEIGHT, buf, font24, TEXT_COLOR, BG_COLOR, justifyLeft);
+                         NUMBERS_WIDTH + COLON_WIDTH, NUMBERS_HEIGHT, buf, font24, TEXT_COLOR, BG_COLOR, justifyLeft);
     }
     if (DT_StructTM.tm_min != DT_StructTM_old.tm_min) {
       sprintf_(buf, "%02u:", DT_StructTM.tm_min);
       DT_StructTM_old.tm_min = DT_StructTM.tm_min;
       gdispFillStringBox(width - NUMBERS_WIDTH * 2 - COLON_WIDTH, (height - NUMBERS_HEIGHT),
-          NUMBERS_WIDTH + COLON_WIDTH, NUMBERS_HEIGHT, buf, font24, TEXT_COLOR, BG_COLOR, justifyLeft);
+                         NUMBERS_WIDTH + COLON_WIDTH, NUMBERS_HEIGHT, buf, font24, TEXT_COLOR, BG_COLOR, justifyLeft);
     }
     if (DT_StructTM.tm_sec != DT_StructTM_old.tm_sec) {
       sprintf_(buf, "%02u ", DT_StructTM.tm_sec);
       DT_StructTM_old.tm_sec = DT_StructTM.tm_sec;
       gdispFillStringBox(width - NUMBERS_WIDTH, (height - NUMBERS_HEIGHT), NUMBERS_WIDTH, NUMBERS_HEIGHT, buf, font24,
-          TEXT_COLOR, BG_COLOR, justifyLeft);
+                         TEXT_COLOR, BG_COLOR, justifyLeft);
     }
 
     static DS18B20_Inner_Val DS_Temp_Vals;
-    static int16_t temp_old = -999;
+    static int16_t temp_old = -99;
 //		msg_t msg;
 //		msg = Core_Module_Update(Temp, NULL, 1000);
     DS_Temp_Vals.temp[0] = -77 << 2;
     Core_Module_Update(Temp, NULL, 1000);
     Core_Module_Read(localhost, Temp, (char*)&DS_Temp_Vals);
-    if ((DS_Temp_Vals.cont_errors > 0))// || (msg != MSG_OK))
-    DS_Temp_Vals.temp[0] = -99 << 2;
+    if ((DS_Temp_Vals.cont_errors > 0))     // || (msg != MSG_OK))
+      DS_Temp_Vals.temp[0] = -99 << 2;
 
     if (temp_old != DS_Temp_Vals.temp[0]) {
       temp_old = DS_Temp_Vals.temp[0];
-      sprintf_(buf, "%02f", (float)DS_Temp_Vals.temp[0]/4);
+      sprintf_(buf, "%02f", (float)DS_Temp_Vals.temp[0] / 4);
       gdispFillStringBox(width - 120, HEIGHT_24, 120, HEIGHT_32, buf, font32, TEXT_COLOR, OT_BG_COLOR, justifyRight);
     }
 
-    eventmask_t evt;
-    uint8_t x;
-    /* Checks if an IRQ happened else wait.*/
-    evt = chEvtWaitOne((eventmask_t)ALL_EVENTS);
-    switch (evt)
-    {
-      case (1):
-      if ((DateTime.millisecond - (DateTime.millisecond%1000))%ALARM_PERIOD_MS == 0)
-      {
-        for (x = 0; x < M5_VALS-1; x++)
-        {
-          gdispDrawPixel(x+80, height - (HEIGHT_24 * 4 + HEIGHT_32 + NUMBERS_HEIGHT)-10-Graph_Data.m5[x].temp[0], TEMP_BG_COLOR);
-          Graph_Data.m5[x]=Graph_Data.m5[x+1];
-          gdispDrawPixel(x+80, height - (HEIGHT_24 * 4 + HEIGHT_32 + NUMBERS_HEIGHT)-10-Graph_Data.m5[x].temp[0], TEXT_1_COLOR);
+    static graph_elem_t Vals_Cur = {}, Vals_old = {}, Vals_prev[2];
+
+    Vals_Cur.temp[2] = DS_Temp_Vals.temp[0] / 4;
+
+    DS_Temp_Vals.temp[0] = -77 << 2;
+//    Core_Module_Update(Temp, NULL, 1000);
+    Core_Module_Read(10, Temp, (char*)&DS_Temp_Vals);
+    if ((DS_Temp_Vals.cont_errors > 0))     // || (msg != MSG_OK))
+      DS_Temp_Vals.temp[0] = -99 << 2;
+
+    //   Vals[0].temp[0] = DS_Temp_Vals.temp[0];
+    //   Vals[0].hum[1] = DS_Temp_Vals.temp[0];
+
+    Vals_Cur.hum[1] = DS_Temp_Vals.temp[0] / 4;
+    Vals_Cur.temp[0] = DS_Temp_Vals.temp[0] / 4;
+
+    uint8_t x, num, val;
+
+    for (val = 0; val < 2; val++) {
+      for (num = 0; num < 3; num++) {
+        if (Vals_old.val[val][num] != Vals_Cur.val[val][num]) {
+          Vals_old.val[val][num] = Vals_Cur.val[val][num];
+          sprintf_(buf, "%02i ", Vals_Cur.val[val][num]);
+//          gdispFillStringBox(width - 120, HEIGHT_24, 120, HEIGHT_32, buf, font32, TEXT_COLOR, OT_BG_COLOR, justifyRight);
+          gdispFillStringBox((width / 2) * (val + 1) - 36 - 5, HEIGHT_24 * (num + 1) + HEIGHT_32, 36, HEIGHT_24, buf,
+                             font24, Text_Color[num], BG_Color[val], justifyRight);
+//          gdispFillStringBox(width / 2, HEIGHT_24 * 2 + HEIGHT_32, width / 2, HEIGHT_24, "Hum 2:", font24, TEXT_2_COLOR,
+//                             HUM_BG_COLOR, justifyLeft);
+//          gdispFillStringBox(width / 2, HEIGHT_24 * 3 + HEIGHT_32, width / 2, HEIGHT_24, "Hum 3:", font24, TEXT_3_COLOR,
+//                             HUM_BG_COLOR, justifyLeft);
         }
-        gdispDrawPixel(x+80, height - (HEIGHT_24 * 4 + HEIGHT_32 + NUMBERS_HEIGHT)-10-Graph_Data.m5[x].temp[0], TEMP_BG_COLOR);
-        Graph_Data.m5[x].temp[0]=DS_Temp_Vals.temp[0]/4;
-        gdispDrawPixel(x+80, height - (HEIGHT_24 * 4 + HEIGHT_32 + NUMBERS_HEIGHT)-10-Graph_Data.m5[x].temp[0], TEXT_1_COLOR);
       }
-      break;
-      case (2):
-      break;
-      default:
-      break;
     }
 
+    if ((DateTime.millisecond - (DateTime.millisecond % 1000)) % ALARM_PERIOD_MS == 0) {
+      bool redraw;
+      do {
+        redraw = FALSE;
+        int16_t max[2] = {-99, -99}, min[2] = {99, 99};
+        static int16_t old_min[2] = {99, 99};
+
+        for (val = 0; val < 2; val++) {
+          for (num = 0; num < 3; num++) {
+            for (x = 0; x < M5_VALS; x++) {
+              if (Graph_Data.m5[x].val[val][num] != 0) {
+                max[val] = MAX(max[val], Graph_Data.m5[x].val[val][num]);
+                min[val] = MIN(min[val], Graph_Data.m5[x].val[val][num]);
+              }
+            }
+            if (Vals_Cur.val[val][num] != 0) {
+              max[val] = MAX(max[val], Vals_Cur.val[val][num]);
+              min[val] = MIN(min[val], Vals_Cur.val[val][num]);
+            }
+          }
+        }
+
+        static int16_t old_range[2] = {1, 1}, range[2];
+        range[0] = 60 / (max[0] - min[0]);
+        range[1] = 60 / (max[1] - min[1]);
+
+        for (val = 0; val < 2; val++) {
+          for (num = 0; num < 3; num++) {
+            Vals_prev[0].val[val][num] = Vals_prev[1].val[val][num] = 0;
+            for (x = 0; x < M5_VALS; x++) {
+              if (Graph_Data.m5[x].val[val][num] != 0) {
+                if (Vals_prev[0].val[val][num] != 0) {
+//                gdispDrawLine( x + 10 + val * (width / 2),
+//                              height - NUMBERS_HEIGHT - 10 - Vals_prev[0].val[val][num] * 8 + 200,
+//                              x + 11 + val * (width / 2),
+//                              height - NUMBERS_HEIGHT - 10 - Graph_Data.m5[x].val[val][num] * 8 + 200, BG_Color[val]);
+                  gdispDrawLine(
+                      x + 10 + val * (width / 2),
+                      height - NUMBERS_HEIGHT - 17 - (Vals_prev[0].val[val][num]-old_min[val])*old_range[val],
+                      x + 11 + val * (width / 2),
+                      height - NUMBERS_HEIGHT - 17 - (Graph_Data.m5[x].val[val][num]-old_min[val])*old_range[val],
+                      BG_Color[val]);
+                }
+                Vals_prev[0].val[val][num] = Graph_Data.m5[x].val[val][num];
+              }
+              if (x < M5_VALS - 1) {
+                Graph_Data.m5[x].val[val][num] = Graph_Data.m5[x + 1].val[val][num];
+              }
+              else {
+                Graph_Data.m5[x].val[val][num] = Vals_Cur.val[val][num];
+              }
+              if (Graph_Data.m5[x].val[val][num] != 0) {
+                if (Vals_prev[1].val[val][num] != 0) {
+//                gdispDrawLine( x + 10 + val * (width / 2),
+//                              height - NUMBERS_HEIGHT - 10 - Vals_prev[1].val[val][num] * 8 + 200,
+//                              x + 11 + val * (width / 2),
+//                              height - NUMBERS_HEIGHT - 10 - Graph_Data.m5[x].val[val][num] * 8 + 200, Text_Color[num]);
+                  gdispDrawLine( x + 10 + val * (width / 2),
+                                height - NUMBERS_HEIGHT - 17 - (Vals_prev[1].val[val][num]-min[val])*range[val],
+                                x + 11 + val * (width / 2),
+                                height - NUMBERS_HEIGHT - 17 - (Graph_Data.m5[x].val[val][num]-min[val])*range[val],
+                                Text_Color[num]);
+                }
+                Vals_prev[1].val[val][num] = Graph_Data.m5[x].val[val][num];
+              }
+            }
+          }
+        }
+
+        if ((old_range[0] != range[0]) || (old_range[1] != range[1])) redraw = TRUE;
+
+        old_min[0] = min[0];
+        old_min[1] = min[1];
+        old_range[0] = range[0];
+        old_range[1] = range[1];
+      } while (redraw);
+    }
     //    time_start = chThdSleepUntilWindowed(time_start, time_start + S2ST(1));
+    chEvtWaitOne((eventmask_t)1);
+
   }
 }
 
--- modules/uGFX/ILI9341/ILI9341.h
+++ modules/uGFX/ILI9341/ILI9341.h
@@ -1,14 +1,21 @@
 #ifndef ILI9341_H_
 #define ILI9341_H_
 
-typedef struct {
-  int8_t temp[3];
-  uint8_t hum[3];
+typedef union {
+  struct {
+    int8_t temp[3];
+    uint8_t hum[3];
+  };
+  int8_t val[2][3];
 } graph_elem_t;
 
-#define M5_VALS 36
-#define H1_VALS 24
-#define D1_VALS 14
+//#define M5_VALS 36
+//#define H1_VALS 24
+//#define D1_VALS 14
+
+#define M5_VALS 110
+#define H1_VALS 1
+#define D1_VALS 1
 
 typedef struct {
   graph_elem_t m5[M5_VALS];
